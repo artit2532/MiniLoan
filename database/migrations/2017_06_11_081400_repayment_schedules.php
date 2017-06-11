84577b29c142e72cef14cfe91282dc63656f6320
Artit Adoonteelasopon <Artit.a@kinofinnovative.com>
2017-06-11 15:26:11 +0700

    create migration file

---
+++ database/migrations/2017_06_11_081400_repayment_schedules.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RepaymentSchedules extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('repayment_schedules')) {
+            Schema::create('repayment_schedules', function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('loan_id');
+                $table->unsignedInteger('payment_no');
+                $table->datetime('date');
+                $table->decimal('payment_amount', 21, 6);
+                $table->decimal('principal', 21, 6);
+                $table->decimal('interest', 21, 6);
+                $table->decimal('balance', 21, 6);
+                $table->datetime('created_at');
+                $table->datetime('updated_at');
+
+                $table->foreign('loan_id')->reference('id')->on('loans');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
